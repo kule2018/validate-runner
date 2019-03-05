30480cd34c45ff7423a96b900bf940ef9c77dc7f
XueYou <xueyoucd@gmail.com>
2019-03-05 11:47:41 +0800

    chore: 更改打包方式

--- src/any-runner.ts
+++ src/any-runner.ts
@@ -1,5 +1,5 @@
-import { ValidateRunner, createInvalidError } from './validate-runner';
-import { ValidateProvider, IMethodRegister } from '@validate/validate-provider';
+import { ValidateRunner, createInvalidError } from "./validate-runner";
+import { ValidateProvider, IMethodRegister } from "@validate/validate-provider";
 
 /**
  * 任意验证执行器
@@ -7,46 +7,52 @@ import { ValidateProvider, IMethodRegister } from '@validate/validate-provider';
  * @description 适用场景: 比如一个输入框既可以输入邮箱也可以输入手机号, 此时邮箱或手机号验证其中一个通过则验证通过
  */
 export class ValidateRunnerAny extends ValidateRunner {
+    /**
+     * 构造函数
+     * @param provider 验证函数提供方
+     */
+    constructor(provider: ValidateProvider) {
+        super(provider);
+    }
 
-  /**
-   * 构造函数
-   * @param provider 验证函数提供方
-   */
-  constructor(provider: ValidateProvider) {
-    super(provider);
-  }
+    /**
+     * 执行验证
+     */
+    public execute(): Promise<IMethodRegister[]> {
+        const { elements, provider } = this;
 
-  /**
-   * 执行验证
-   */
-  public execute(): Promise<IMethodRegister[]> {
-    const { elements, provider } = this;
+        return new Promise((resolve, reject) => {
+            try {
+                this.makeSureProvider();
+            } catch (error) {
+                reject(error);
+                return;
+            }
 
-    return new Promise((resolve, reject) => {
-
-      try {
-        this.makeSureProvider();
-      } catch (error) {
-        reject(error);
-        return;
-      }
-
-      let successd = false;
-      let failCount = 0;
-      for (var i = 0; i < elements.length; ++i) {
-        if (successd) { break; }
-        var runnerElement = elements[i];
-        var provideInfo = provider.get(runnerElement.name);
-        if (!provideInfo) { reject(createInvalidError(runnerElement.name)); }
-        provideInfo.method(runnerElement.value, ...runnerElement.params).then((result) => {
-          successd = true;
-          resolve([result])
-        }).catch(error => {
-          ++failCount;
-          if (failCount >= elements.length) { reject(error); }
-        })
-      }
-    });
-  }
-
+            let successd = false;
+            let failCount = 0;
+            for (var i = 0; i < elements.length; ++i) {
+                if (successd) {
+                    break;
+                }
+                var runnerElement = elements[i];
+                var provideInfo = provider.get(runnerElement.name);
+                if (!provideInfo) {
+                    reject(createInvalidError(runnerElement.name));
+                }
+                provideInfo
+                    .method(runnerElement.value, ...runnerElement.params)
+                    .then((result) => {
+                        successd = true;
+                        resolve([result]);
+                    })
+                    .catch((error) => {
+                        ++failCount;
+                        if (failCount >= elements.length) {
+                            reject(error);
+                        }
+                    });
+            }
+        });
+    }
 }
